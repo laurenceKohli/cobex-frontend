875acf706530ad136cb622bf1d31d5ba950c5db4
laurenceKohli <laurence.kohli@hes-so.com>
2024-12-09 13:36:17 +0100

    Composants InbutText et InputLabel

---
+++ src/frontend/components/BaseInputLabel.vue
@@ -0,0 +1,18 @@
+<script setup>
+const props = defineProps({
+    value: {
+        type: String,
+    },
+})
+</script>
+
+<template>
+    <label>
+        <span v-if="value">{{ value }}</span>
+        <span v-else><slot /></span>
+    </label>
+</template>
+
+<style scoped>
+
+</style>
--- src/frontend/components/TheHome.vue
+++ src/frontend/components/TheHome.vue
@@ -1,11 +1,15 @@
 <script setup>
 import BaseButton from './BaseButton.vue'
+import BaseInputLabel from './BaseInputLabel.vue';
+import BaseInput from './BaseInput.vue';
 </script>
 
 <template>
     <BaseButton icon="🏠">
         Home
     </BaseButton>
+    <BaseInputLabel>Search</BaseInputLabel>
+    <BaseInput placeholder="Search" />
 </template>
 
 <style scoped>
