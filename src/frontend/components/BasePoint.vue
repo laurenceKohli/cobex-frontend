a071a4216e003ae049f02b2175494166de5db359
laurenceKohli <laurence.kohli@hes-so.com>
2025-01-04 11:53:58 +0100

    CSS BasePoint

--- src/frontend/components/BasePoint.vue
+++ src/frontend/components/BasePoint.vue
@@ -1,22 +1,33 @@
 <script setup>
-    import { onMounted } from 'vue';
+import { ref } from 'vue';
 
- const props = defineProps({
-     point: String
- })
+const props = defineProps({
+    point: String,
+    isActive: Boolean,
+})
 
- onMounted(() => {
-     console.log(point);
- })
+const active = ref('');
+if (props.isActive) {
+    active.value = 'active';
+}
 
 </script>
 
 <template>
     <div id="point">
-        <div class="point" :style="{left: point.x + 'px', top: point.y + 'px'}"></div>
+        <div class="point" :style="{ left: point.x + 'px', top: point.y + 'px' }">
+            <span class="material-symbols-outlined" :class="active">location_on</span>
+        </div>
     </div>
 </template>
 
 <style scoped>
+span {
+    font-size: 2rem;
+    color: var(--color-facile);
+}
 
+.active {
+    color: var(--color-primary);
+}
 </style>
