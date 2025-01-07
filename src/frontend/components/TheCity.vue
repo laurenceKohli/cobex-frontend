d18be8615952ac8911f9e99b6732b7057398c8c2
Odin Wermeille <odin.wermeille@heig-vd.ch>
2025-01-07 09:02:59 +0100

    ajout des filtres à la page City

--- src/frontend/components/TheCity.vue
+++ src/frontend/components/TheCity.vue
@@ -2,9 +2,13 @@
 import {  useFetchApiCrud } from '../composables/useFetchApiCrud';
 import BaseCard from './BaseCard.vue';
 import BaseButton from './BaseButton.vue';
-import { compile, ref, watch } from 'vue';
+import { compile, computed, ref, watch } from 'vue';
 import { currentTrail } from '../stores/utils';
 import { difficultySort } from '../composables/difficultySort';
+// import { ref } from 'vue';
+
+const filterDifficulty = ref('tout');
+const showFilterModal = ref(false);
 
  const ville = window.location.href.split('#')[1];
  const villeCapitalized = ville.charAt(0).toUpperCase() + ville.slice(1);
@@ -15,20 +19,17 @@ import { difficultySort } from '../composables/difficultySort';
  //let dataCopy = [...data];
  let nomVille = '';
  let link = ref([]);
- let sorted = false;
+ const sorted = ref("asc");
 
-const toggleSortDifficulty = () => {
-    if(!sorted || sorted === "desc"){
-        data.value.sort(difficultySort);
-        sorted = "asc";
-    } else if(sorted === "asc") {
-        data.value.sort(difficultySort).reverse();
-        sorted = "desc";
-    
-    } /*else {
-        sorted = false;
-    }*/
-}
+const toggleSortDifficulty = () => sorted.value === "asc" ? sorted.value = "desc" : sorted.value = "asc";
+    // if(sorted === "desc"){
+    //     parcoursFiltres.value.sort(difficultySort);
+    //     sorted = "asc";
+    // } else if(sorted === "asc") {
+    //     parcoursFiltres.value.sort(difficultySort).reverse();
+    //     sorted = "desc";
+    // }
+// }
 
  switch(ville){
      case 'yverdon':
@@ -38,14 +39,51 @@ const toggleSortDifficulty = () => {
         nomVille = villeCapitalized;
         break;
  }
- console.log(ville);
 
  //si link change alors on va sur la page du parcours
-    watch(link, (newValue) => {
-        console.log(newValue);
-        currentTrail.value = newValue;
-        window.location.href = `#parcours-detail`;
-    });
+watch(link, (newValue) => {
+    console.log(newValue);
+    currentTrail.value = newValue;
+    window.location.href = `#parcours-detail`;
+});
+
+const showModal = () => {
+    showFilterModal.value = true;
+    console.log(showFilterModal.value);
+}
+
+const applyFilters = () => {
+    closeModal();
+}
+
+const resetFilters = () => {
+    filterDifficulty.value = 'tout';
+    closeModal();
+}
+
+const closeModal = () => {
+    showFilterModal.value = false;
+}
+
+watch(filterDifficulty, (newValue) => {
+    console.log("diff:"+newValue);
+});
+
+const parcoursFiltres = computed(() => {
+    if (filterDifficulty.value === 'tout') return data.value;
+    return data.value.filter((course) => course.difficulte === filterDifficulty.value);
+});
+
+const sortedParcours = computed(() => {
+    if(sorted.value === "asc"){
+        return parcoursFiltres.value?.toSorted(difficultySort);
+    } else if(sorted.value === "desc") {
+        return parcoursFiltres.value?.toSorted((a,b) => -difficultySort(a, b));
+    }
+});
+
+
+
 </script>
 
 <template>
@@ -56,7 +94,10 @@ const toggleSortDifficulty = () => {
         <BaseButton @click="toggleSortDifficulty" class="secondary">
             Trier par difficulté
         </BaseButton>
-        <template v-for="parcours in data">
+        <BaseButton @click="showModal" class="secondary">
+            Filtrer
+        </BaseButton>
+        <template v-for="parcours in sortedParcours">
                 <BaseCard
                     :info="parcours"
                     @click="link = parcours.id"
@@ -65,6 +106,47 @@ const toggleSortDifficulty = () => {
     </div>
     <div v-else>
         <p>Cette ville n'a pas encore de parcours.</p>
+    </div>
+
+    <div v-if="showFilterModal" class="modalBackdrop" @click="closeModal">
+        <div class="modal" @click.stop>
+            <div class="modalContent">
+                <div class="modalHeader">
+                    <h3>Filtrer par</h3>
+                </div>
+                <div>
+                    <div>
+                        <h4><strong>Difficulté</strong></h4>
+                        <div class="radioGroup">
+                            <label>
+                                <input type="radio" name="difficulty" value="tout" v-model="filterDifficulty" />
+                                Tout
+                            </label>
+                            <label>
+                                <input type="radio" name="difficulty" value="facile" v-model="filterDifficulty" />
+                                Facile
+                            </label>
+                            <label>
+                                <input type="radio" name="difficulty" value="moyen" v-model="filterDifficulty" />
+                                Moyen
+                            </label>
+                            <label>
+                                <input type="radio" name="difficulty" value="difficile" v-model="filterDifficulty" />
+                                Difficile
+                            </label>
+                            <label>
+                                <input type="radio" name="difficulty" value="très difficile" v-model="filterDifficulty" />
+                                Très difficile
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="modalFooter">
+                    <BaseButton class="Button" @click="resetFilters">RÉINITIALISER</BaseButton>
+                    <BaseButton @click="applyFilters">VALIDER</BaseButton>
+                </div>
+            </div>
+        </div>
     </div>
     
 </template>
@@ -76,5 +158,24 @@ div.trailsList {
     gap: var(--spacing-large);
     flex-wrap: wrap;
     margin-top: 14px;
+}
+
+.modal {
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  padding-top: 100px; /* Location of the box */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+}
+
+.modalContent {
+  background-color: #fefefe;
+  margin: auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
 }
 </style>
